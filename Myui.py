d115619ee0dcf6e6a804139adb64f971bc043088
lyhlyhl <lyhlyhl@qq.com>
2020-07-08 22:41:02 +0800

    停电更新

--- Myui.py
+++ Myui.py
@@ -6,57 +6,57 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import threading
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QTextEdit, QTextBrowser, QHBoxLayout, QVBoxLayout,QMainWindow,QVBoxLayout,QLineEdit,QFormLayout,QPushButton
+from PyQt5.QtWidgets import QApplication,QMessageBox, QWidget, QLabel, QTextEdit, QTextBrowser, QHBoxLayout, QVBoxLayout,QMainWindow,QVBoxLayout,QLineEdit,QFormLayout,QPushButton
 
 class Ui_start(qtui.Ui_MainWindow): #定义一个ui类继承Qt Designer生成的类
     def __init__(self,Mainwindow):
         super().setupUi(Mainwindow) #初始化窗口
-       # self.Mainwindow = Mainwindow
+        self.Mainwindow = Mainwindow
         #self.action_2.clicked.connect(self.action2_solt)
-        Mainwindow.setWindowIcon(QIcon('myico.ico'))    #设置窗口的图标
-        Mainwindow.resize(300,300)
-        self.label1 = QLabel(Mainwindow)  # 开始界面为窗口的说明界面 里面的控件及其排布
-        self.TextBrowser1 = QTextBrowser(Mainwindow)
+        self.Mainwindow.setWindowIcon(QIcon('myico.ico'))    #设置窗口的图标
+        self.Mainwindow.resize(300,300)
+        self.label1 = QLabel(self.Mainwindow)  # 开始界面为窗口的说明界面 里面的控件及其排布
+        self.TextBrowser1 = QTextBrowser(self.Mainwindow)
         self.label1.setText("<h2>使用说明</h2>")
         self.TextBrowser1.setStyleSheet("QTextBrowser{border-width:0;border-style:outset}")
         self.TextBrowser1.setText("1.在类别中选择自己想要打的副本\n"
                                   "2.点击")
 
-        self.action_3.triggered.connect(lambda :self.action3_solt(Mainwindow)) #建立菜单栏和槽函数的connect
-        self.layout_init(Mainwindow)#布局
-    def layout_init(self,Mainwindow): #控件的排布函数
+        self.action_3.triggered.connect(lambda :self.action3_solt(self.Mainwindow)) #建立菜单栏和槽函数的connect
+        self.layout_init()#布局
+    def layout_init(self): #控件的排布函数
         self.widget = QWidget()
         self.v_layout = QVBoxLayout()
         self.v_layout.addWidget(self.label1)
         self.v_layout.addWidget(self.TextBrowser1)
         self.widget.setLayout(self.v_layout)
-        Mainwindow.setCentralWidget(self.widget)
+        self.Mainwindow.setCentralWidget(self.widget)
 
     def action3_solt(self,Mainwindow):  #双人的菜单点击槽函数
         self.widget.setParent(None)
-        label1 = QLabel("窗口1的句柄",Mainwindow)
-        label2 = QLabel("窗口2的句柄",Mainwindow)
-        label3 = QLabel("hwnd的值为: ",Mainwindow)
+        label1 = QLabel("窗口1的句柄",self.Mainwindow)
+        label2 = QLabel("窗口2的句柄",self.Mainwindow)
+        label3 = QLabel("hwnd的值为: ",self.Mainwindow)
 
         # = option.GetMousePosition()
-        label4 = QLabel("暂无数据",Mainwindow)
-        label5 = QLabel("进程标题： ",Mainwindow)
-        label6 = QLabel("暂无数据", Mainwindow)
+        label4 = QLabel("暂无数据",self.Mainwindow)
+        label5 = QLabel("进程标题： ",self.Mainwindow)
+        label6 = QLabel("暂无数据",self.Mainwindow)
 
         t = threading.Thread(target=lambda: thead_SetHwndLabel(label4, label6))
         t.start()
-        label7 = QLabel("每轮时间", Mainwindow)
+        label7 = QLabel("每轮时间", self.Mainwindow)
 
-        line1 = QLineEdit(Mainwindow)
+        line1 = QLineEdit(self.Mainwindow)
         line1.setMaximumWidth(100)
-        line2 = QLineEdit(Mainwindow)
+        line2 = QLineEdit(self.Mainwindow)
         line2.setMaximumWidth(100)
-        line3 = QLineEdit(Mainwindow)
+        line3 = QLineEdit(self.Mainwindow)
         line3.setMaximumWidth(100)
 
-        confirm_button = QPushButton('开始', Mainwindow)
+        confirm_button = QPushButton('开始', self.Mainwindow)
         confirm_button.clicked.connect(lambda :self.action3_confirm(line1,line2,line3))
-        cancel_button = QPushButton('停止', Mainwindow)
+        cancel_button = QPushButton('停止', self.Mainwindow)
 
         f_layout = QFormLayout()  # 1
         s_layout = QHBoxLayout()
@@ -79,18 +79,24 @@ class Ui_start(qtui.Ui_MainWindow): #定义一个ui类继承Qt Designer生成的
         all_v_layout.addLayout(button_layout)
         widget = QWidget()
         widget.setLayout(all_v_layout)
-        Mainwindow.setCentralWidget(widget)
+        self.Mainwindow.setCentralWidget(widget)
 
 
     def action3_confirm(self,line1,line2,line3):
         hwnd1 = line1.text()
         hwnd2 = line2.text()
         Turntime = line3.text()
-        if hwnd1 != None and hwnd2 !=None and Turntime != None:
+        print(hwnd1)
+        print(hwnd2)
+        print(Turntime)
+        print(1)
+        if hwnd1 != "" and hwnd2 !="" and Turntime != "":
             windows1=option.MyWindows(int(hwnd1))
             windows2=option.MyWindows(int(hwnd2))
             windows1.ChangeWindows(10,10,300,300)
         else:
+            QMessageBox.information(self.Mainwindow,'提示','请勿输入空内容')
+
             
 def thead_SetHwndLabel(label1,label2):
     while 1:
